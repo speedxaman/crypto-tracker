a85f4fae7346ef7caf41295737f78ac551549c36
Aman Gautam <116288913+speedxaman@users.noreply.github.com>
2025-07-01 19:48:23 +0530

    api commit

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pro-api.coinmarketcap.com/v1',
-  apiKey: 'YOUR_COINMARKETCAP_API_KEY' // Replace with actual API key
+  apiKey: 'apikey' 
 };
